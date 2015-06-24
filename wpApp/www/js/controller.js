90dac555f5a3a14a27b55cdf0cb51ea17b9fc20f
Allyson Alves de Souza <allyson@hastedesign.com.br>
2015-06-24 16:10:58 -0300

    Alterado url de get

--- wpApp/www/js/controller.js
+++ wpApp/www/js/controller.js
@@ -1,8 +1,7 @@
 wpApp.controller('DashCtrl', function($scope, $http) {
-	$http.get('http://127.0.0.1/Web/side/allysonsouza/wp-json/posts?type=conteudo-didatico')
+	$http.get('http://127.0.0.1/Web/side/wpapp/wordpress/wp-json/posts')
 	.success( function(data) {
 		$scope.title = 'Palestras WordCamp São Paulo 2015';
 		$scope.posts = data;
 	})
 })
-
