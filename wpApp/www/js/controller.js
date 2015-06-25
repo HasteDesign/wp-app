20b1cc57f4cd2b0425c15acd4eddce2dc4f541b4
Allyson Alves de Souza <allyson@hastedesign.com.br>
2015-06-25 03:01:14 -0300

    Modificação do conteúdo do controller para retornar todas as palestras ( filter[posts_per_page]=-1 )

--- wpApp/www/js/controller.js
+++ wpApp/www/js/controller.js
@@ -1,5 +1,5 @@
 wpApp.controller('DashCtrl', function($scope, $http) {
-	$http.get('http://127.0.0.1/Workspace/side/wpapp/wordpress/wp-json/posts')
+	$http.get('http://127.0.0.1/Workspace/side/wpapp/wordpress/wp-json/posts?type=palestra&filter[posts_per_page]=-1')
 	.success( function(data) {
 		$scope.title = 'Palestras WordCamp São Paulo 2015';
 		$scope.posts = data;
