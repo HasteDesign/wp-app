ea2dac8949612110641d050de8558506c356b6c5
Allyson Alves de Souza <allyson@hastedesign.com.br>
2015-06-25 02:41:10 -0300

    Corrigido endereço de requisição do JSON (web/Workspace) no controller.js

--- wpApp/www/js/controller.js
+++ wpApp/www/js/controller.js
@@ -1,5 +1,5 @@
 wpApp.controller('DashCtrl', function($scope, $http) {
-	$http.get('http://127.0.0.1/Web/side/wpapp/wordpress/wp-json/posts')
+	$http.get('http://127.0.0.1/Workspace/side/wpapp/wordpress/wp-json/posts')
 	.success( function(data) {
 		$scope.title = 'Palestras WordCamp São Paulo 2015';
 		$scope.posts = data;
